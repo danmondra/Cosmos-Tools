85b30ab54f98fb90ab5e41f451e69d4eab4977dd
danmondra <danimonta15@gmail.com>
2023-02-27 21:09:57 -0600

    Create list of constellations objects and them stars

---
+++ app/data/constellations.js
@@ -0,0 +1,119 @@
+const constellations = [
+  {
+    name: 'Can Mayor',
+    id: 'canmayor',
+    description: 'Constellation of imaging tools',
+    rol: 'Images',
+    stars: [
+      {
+        name: 'Sirius',
+        description: 'Image resizer',
+        id: 'sirius',
+        url: 'image-resizer',
+        color: '#fff',
+        shadow: '#fff',
+        width: '8rem',
+        position: { top: '10%', left: '25%' }
+      },
+      {
+        name: 'VY Canis Majoris',
+        description: 'Image cropper',
+        id: 'vycanismajoris',
+        url: 'image-cropper',
+        color: '#9C5A1C',
+        shadow: '#C50000',
+        width: '10rem',
+        position: { top: '3%', left: '60%' }
+      },
+      {
+        name: 'Adhara',
+        description: 'Image scaler',
+        id: 'adhara',
+        url: 'image-scaler',
+        color: '#8BA5E9',
+        shadow: '#3A69C6',
+        width: '5rem',
+        position: { top: '0%', left: '40%' }
+      }
+    ]
+  },
+  {
+    name: 'Andromeda',
+    id: 'andromeda',
+    description: 'Constellation of tools for creators',
+    rol: 'Creators',
+    stars: [
+      {
+        name: 'Alpheratz',
+        description: 'Thumbnail creator',
+        id: 'alpheratz',
+        url: 'thumbnail-creator',
+        color: '#fff',
+        shadow: '#fff',
+        width: '8rem',
+        position: { top: '10%', left: '25%' }
+      },
+      {
+        name: 'Mirach',
+        description: 'Background Remover',
+        id: 'mirach',
+        url: 'background-remover',
+        color: '#9C5A1C',
+        shadow: '#C50000',
+        width: '10rem',
+        position: { top: '3%', left: '60%' }
+      },
+      {
+        name: 'Almach',
+        description: 'Celebrity Detection',
+        id: 'almach',
+        url: 'celebrity-detection',
+        color: '#8BA5E9',
+        shadow: '#3A69C6',
+        width: '5rem',
+        position: { top: '0%', left: '40%' }
+      }
+    ]
+  },
+  {
+    name: 'Pegasus',
+    id: 'pegasus',
+    description: 'Constellation of video tools',
+    rol: 'Videos',
+    stars: [
+      {
+        name: 'Enif',
+        description: 'Video cropper',
+        id: 'enif',
+        url: 'video-cropper',
+        color: '#fff',
+        shadow: '#fff',
+        width: '8rem',
+        position: { top: '10%', left: '25%' }
+      },
+      {
+        name: 'Scheat',
+        description: 'Video Transcriptor',
+        id: 'scheat',
+        url: 'background-remover',
+        color: '#9C5A1C',
+        shadow: '#C50000',
+        width: '10rem',
+        position: { top: '3%', left: '60%' }
+      },
+      {
+        name: 'Markab',
+        description: 'Reducer video size',
+        id: 'markab',
+        url: 'celebrity-detection',
+        color: '#8BA5E9',
+        shadow: '#3A69C6',
+        width: '5rem',
+        position: { top: '0%', left: '40%' }
+      }
+    ]
+  }
+
+]
+
+export default constellations
