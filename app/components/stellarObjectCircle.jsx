f77f122f7f4c6c264fc4bd5f67f6d68544796f3c
danmondra <danimonta15@gmail.com>
2023-02-27 21:19:11 -0600

    Add props to put styles inline

--- app/components/stellarObjectCircle.jsx
+++ app/components/stellarObjectCircle.jsx
@@ -1,6 +1,16 @@
-export function StellarObjectCircle({ id }) {
+export function StellarObjectCircle({ star }) {
+  const { color, shadow } = star
+
   return (
-    <svg viewBox='0 0 100 100' xmlns='http://www.w3.org/2000/svg' className='star' data-stellarobject={id}>
+    <svg
+      viewBox='0 0 100 100'
+      xmlns='http://www.w3.org/2000/svg'
+      className='star'
+      style={{
+        fill: color,
+        filter: `drop-shadow(0 0 6px ${shadow})`
+      }}
+    >
       <circle cx='50' cy='50' r='50' />
     </svg>
   )
