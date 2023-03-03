0aa84f0b7ba9401969940e8c9b92dd420b16959a
danmondra <danimonta15@gmail.com>
2023-03-03 15:04:03 -0600

    Create buttons to go to home and download file

---
+++ app/components/btnDownload.jsx
@@ -0,0 +1,5 @@
+export function BtnDownload({ text }) {
+  return (
+    <input type='submit' value={text} className='btnDownload' />
+  )
+}
---
+++ app/components/btnHome.jsx
@@ -0,0 +1,12 @@
+import { Link } from '@remix-run/react'
+
+export function HomeBtn() {
+  return (
+    <div>
+      <Link to='/' className='homeIcon'>
+        <svg xmlns='http://www.w3.org/2000/svg' height='48' viewBox='0 96 960 960' width='48'><path d='M220 876h150V626h220v250h150V486L480 291 220 486v390Zm-60 60V456l320-240 320 240v480H530V686H430v250H160Zm320-353Z' fill='#fff' /></svg>
+      </Link>
+    </div>
+
+  )
+}
