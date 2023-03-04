69820638e0b3b32d6e7d92f06dae814e73bcd784
danmondra <danimonta15@gmail.com>
2023-03-04 15:00:50 -0600

    Add logic to download files

--- app/components/btnDownload.jsx
+++ app/components/btnDownload.jsx
@@ -1,5 +1,41 @@
-export function BtnDownload({ text }) {
+export function BtnDownload({ text, files }) {
+  async function getFileBlobUrl(url) {
+    const file = await fetch(url)
+    const fileBlob = await file.blob()
+    const fileUrl = URL.createObjectURL(fileBlob)
+
+    return fileUrl
+  }
+
+  async function handleDownload(e) {
+    e.preventDefault()
+
+    let toDownload = [files]
+
+    if (files.length > 0) {
+      toDownload = [...files]
+    }
+
+    toDownload.forEach(async ({ secure_url, original_filename }) => {
+      const fileUrl = await getFileBlobUrl(secure_url)
+
+      const downloadLink = document.createElement('a')
+      downloadLink.href = fileUrl
+      downloadLink.download = original_filename
+
+      document.body.appendChild(downloadLink)
+      downloadLink.click()
+      document.body.removeChild(downloadLink)
+
+      URL.revokeObjectURL(fileUrl)
+    })
+  }
+
   return (
-    <input type='submit' value={text} className='btnDownload' />
+    <button
+      className='btnDownload'
+      onClick={handleDownload}
+    >{text}
+    </button>
   )
 }
